f4c67eec60f32b8bc223fdc04340ead71b388096
rockstar <rockstar@anapnea.net>
2010-05-16 09:17:16 +0100

    new file:   4.c

---
+++ examples/1/4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* printf Fahrenheit-Celsius table */
+
+int
+main(void)
+{
+    int fahr;
+
+    for (fahr = 0; far <= 300; fahr = fahr + 20)
+        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    return 0;
+}
