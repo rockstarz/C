e3e3e381b5a4e35dc3d0548f055d25356fbc26ea
rockstar <rockstar@anapnea.net>
2010-05-16 06:26:10 +0100

    3.c

--- examples/1/3.c
+++ examples/1/3.c
@@ -9,6 +9,10 @@ main(void)
     float fahr, celsius;
     int lower, upper, step;
 
+    lower = 0;
+    upper = 300;
+    step = 20;
+
     fahr = lower;
     while (fahr <= upper) {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
